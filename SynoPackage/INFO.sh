896aa0d18ef887c1b56e9932fd3dbb645c0fe432
Ingo Theiss <ingo.theiss@i-matrixx.de>
2018-01-31 13:44:37 +0000

    Fix wrong icon size and small improvements

--- SynoPackage/INFO.sh
+++ SynoPackage/INFO.sh
@@ -4,20 +4,22 @@ source /pkgscripts/include/pkg_util.sh
 
 build_number="$1"
 
+cat <<EOF
 package="openHAB"
-version="2.3.0-${build_number}"
 displayname="openHAB"
+version="2.3.0-${build_number}"
+firmware="6.0-7312"
+arch="noarch"
+beta="yes"
 maintainer="Ingo Theiss"
 maintainer_url="https://github.com/itheiss/openhab-synology-spk"
-arch="noarch"
-os_min_ver="6.0"
-beta="yes"
 adminprotocol="https"
 adminurl="/start/index"
 adminport="48443"
 install_dep_packages="Java8"
 install_replace_packages="openHAB<2.3.0"
 helpurl="https://github.com/itheiss/openhab-synology-spk"
+report_url="https://github.com/itheiss/openhab-synology-spk"
 silent_install="yes"
 silent_upgrade="yes"
 dsmuidir="ui"
@@ -25,5 +27,12 @@ dsmappname="ORG.OPENHAB.Instance"
 description="openHAB - a vendor and technology agnostic open source automation software for your home."
 description_enu="openHAB - a vendor and technology agnostic open source automation software for your home."
 description_ger="openHAB - eine Hersteller- und technologieunabhängige Open-Source-Automatisierungssoftware für Ihr Zuhause."
-[ "$(caller)" != "0 NULL" ] && return 0
-pkg_dump_info
+create_time="$(date +%Y%m%d-%T)"
+toolkit_version="${DSM_BUILD_NUM}"
+EOF
+#
+# Disabled until usage is documented.
+# @see https://github.com/SynologyOpenSource/pkgscripts-ng/issues/4
+#[ "$(caller)" != "0 NULL" ] && return 0
+#pkg_dump_info
+#
